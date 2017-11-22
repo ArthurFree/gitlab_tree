15dd54ef6f3cfa0397df73ef1cdf1a042c81f54f
ArthurFree <lc940401@gmail.com>
2017-11-22 10:30:07 +0800

    remove dist

--- src/stores/test.ts
+++ src/stores/test.ts
@@ -1,16 +1,19 @@
 import { observable, action } from 'mobx';
 import utils from '../utils';
+import { GetTopicParam } from '../interface';
 
 class Test {
     @observable list: any = [];
 
     @action getTree(cfg?: any) {
+        let params: GetTopicParam = {
+            page: 1,
+            limit: 10,
+        };
         utils.ajax({
             baseURL: 'https://cnodejs.org/api/v1',
             url: '/topics',
-            params: {
-
-            }
-        })
+            params,
+        });
     }
 }
